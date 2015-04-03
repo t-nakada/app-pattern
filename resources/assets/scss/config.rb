cea2eafaa1ab04bb1eb29ed43c0ae6c28763ecd0
Takashi Nakada <nakada@wizgeek.co.jp>
2015-04-03 10:54:58 +0900

    initial commit

---
+++ resources/assets/scss/config.rb
@@ -0,0 +1,28 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "files"
+css_dir = "public/css"
+sass_dir = "resources/assets/scss"
+images_dir = "public/img"
+fonts_dir = "public/font"
+javascripts_dir = "public/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+#output_style = :expanded
+output_style = :compressed
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+cache = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+
